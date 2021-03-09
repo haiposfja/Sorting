d3c3ec251d2773dd8caccf269a48371e20fcf102
haiposfja <79170447+haiposfja@users.noreply.github.com>
2021-03-09 11:05:27 -0500

    Update StudentSorter.java

--- StudentSorter.java
+++ StudentSorter.java
@@ -9,16 +9,16 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class StudentSorter {
-	private final static int TBD = 35;
-	private String[] students = new String[TBD];
-	private String[] scores = new String[TBD];
+	private final static int LENGTH = 35;
+	private String[] students = new String[LENGTH];
+	private String[] scores = new String[LENGTH];
 
 	public StudentSorter() throws IOException {
 		File studentsNScores = new File("A-71.txt");
 		Scanner reader = new Scanner(studentsNScores);
 			//reads information from the file
 		while (reader.hasNextLine()) {
-			for (int i = 0; i < TBD; i++) {
+			for (int i = 0; i < LENGTH; i++) {
 				String studentInfo = reader.nextLine(); //stores the current name into a string
 				String markInfo = reader.nextLine(); //stores the current mark into a string
 				students[i] = studentInfo;
@@ -39,11 +39,11 @@ public class StudentSorter {
 		String[] alphabetized = new String[TBD];
 		
 			// copies both student array and score array to the local array
-		for (int i = 0; i < TBD; i++) {
+		for (int i = 0; i < LENGTH; i++) {
 			alphabetized[i] = students[i] + "," + scores[i];
 		}
 			//for loop to sort
-		for (int x = 1; x < TBD; x++) {
+		for (int x = 1; x < LENGTH; x++) {
 			String currentName = alphabetized[x].split(",")[0]; //stores the current name in a string
 			String currentMark = alphabetized[x].split(",")[1]; //stores the current mark in a string
 			int y = x - 1; //defines y each loop as one less than x
@@ -64,11 +64,11 @@ public class StudentSorter {
 		String[] numericalized = new String[TBD];
 
 			// copies both student array and score array to the local array
-		for (int i = 0; i < TBD; i++) {
+		for (int i = 0; i < LENGTH; i++) {
 			numericalized[i] = students[i] + "," + scores[i];
 		}
 			//for loop to sort
-		for (int x = 1; x < TBD; x++) {
+		for (int x = 1; x < LENGTH; x++) {
 			String currentName = numericalized[x].split(",")[0]; //stores the current name in a String
 			String currentMark = numericalized[x].split(",")[1]; //stores the current score in a String
 			int y = x - 1; //defines y each loop as one less than x
@@ -86,13 +86,15 @@ public class StudentSorter {
 
 	//method to display the data: uses one for loop and two enhanced for loops
 	public void displayData() {
+		//prints the original data from the file
 		System.out.println("Original Data from the File: \n");
 		System.out.println("Score|Student");
-		for (int i = 0; i < TBD; i++) {
+		for (int i = 0; i < LENGTH; i++) {
 			System.out.print(scores[i] + "     ");
 			System.out.println(students[i]);
 		}
 		
+		//prints the data in an alphabetized format
 		System.out.println("\n\n\n\n");
 		System.out.println("Sorted Alphabetically: A-Z: \n");
 		System.out.println("Score|Student");
@@ -100,7 +102,8 @@ public class StudentSorter {
 			System.out.print(str.split(",")[1] + "     ");
 			System.out.println(str.split(",")[0]);
 		}
-
+		
+		//prints the data in descending order (marks)
 		System.out.println("\n\n\n\n");
 		System.out.println("Sorted Numerically: Highest Average to Lowest Average: \n");
 		System.out.println("Score|Student");
