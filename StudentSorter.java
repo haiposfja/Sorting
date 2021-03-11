8f6240acd9337e5a39408378c1af81b3c76a86e5
haiposfja <79170447+haiposfja@users.noreply.github.com>
2021-03-10 19:41:51 -0500

    Update StudentSorter.java

--- StudentSorter.java
+++ StudentSorter.java
@@ -2,6 +2,7 @@
  * Ms. Krasteva
  * 3/9/2021
  * This program will store and sort the data from A-71.txt
+ * Print Order: Marks --> Name to keep equal distance between strings without the use of stringbuilder
  */
 
 import java.io.File;
@@ -16,11 +17,11 @@ public class StudentSorter {
 	public StudentSorter() throws IOException {
 		File studentsNScores = new File("A-71.txt");
 		Scanner reader = new Scanner(studentsNScores);
-			//reads information from the file
+		// reads information from the file
 		while (reader.hasNextLine()) {
 			for (int i = 0; i < LENGTH; i++) {
-				String studentInfo = reader.nextLine(); //stores the current name into a string
-				String markInfo = reader.nextLine(); //stores the current mark into a string
+				String studentInfo = reader.nextLine(); // stores the current name into a string
+				String markInfo = reader.nextLine(); // stores the current mark into a string
 				students[i] = studentInfo;
 				scores[i] = markInfo;
 			}
@@ -30,82 +31,90 @@ public class StudentSorter {
 	}
 
 	// --------- SORTING USED: INSERTION SORT ---------//
-	
-	
-	//sorts alphabetically: A-Z
+
+	// sorts alphabetically: A-Z
 	public String[] sortAlphabetically() {
-		
-			// local array to prevent changing the original array
+
+		// local array to prevent changing the original array
 		String[] alphabetized = new String[LENGTH];
-		
-			// copies both student array and score array to the local array
+
+		// copies both student array and score array to the local array
 		for (int i = 0; i < LENGTH; i++) {
 			alphabetized[i] = students[i] + "," + scores[i];
 		}
-			//for loop to sort
+		// for loop to sort
 		for (int x = 1; x < LENGTH; x++) {
-			String currentName = alphabetized[x].split(",")[0]; //stores the current name in a string
-			String currentMark = alphabetized[x].split(",")[1]; //stores the current mark in a string
-			int y = x - 1; //defines y each loop as one less than x
-				//shifts the elements that are alphabetically larger than currentName one to the right
+			String currentName = alphabetized[x].split(",")[0]; // stores the current name in a string
+			String currentMark = alphabetized[x].split(",")[1]; // stores the current mark in a string
+			int y = x - 1; // defines y each loop as one less than x
+			// shifts the elements that are alphabetically larger than currentName one to
+			// the right
 			while ((y > -1) && alphabetized[y].split(",")[0].compareTo(currentName) > 0) {
-				alphabetized[y + 1] = alphabetized[y]; 
-				y--; 
+				alphabetized[y + 1] = alphabetized[y];
+				y--;
 			}
-			alphabetized[y + 1] = currentName + "," + currentMark; //inserts
+			alphabetized[y + 1] = currentName + "," + currentMark; // inserts
 		}
-		return alphabetized; //returns sorted array
+		return alphabetized; // returns sorted array
 	}
 
-	//sorts numerically: descending order
+	// sorts numerically: descending order
 	public String[] sortNumerically() {
-		
-			// local array to prevent changing the original array
+
+		// local array to prevent changing the original array
 		String[] numericalized = new String[LENGTH];
 
-			// copies both student array and score array to the local array
+		// copies both student array and score array to the local array
 		for (int i = 0; i < LENGTH; i++) {
 			numericalized[i] = students[i] + "," + scores[i];
 		}
-			//for loop to sort
+		// for loop to sort
 		for (int x = 1; x < LENGTH; x++) {
-			String currentName = numericalized[x].split(",")[0]; //stores the current name in a String
-			String currentMark = numericalized[x].split(",")[1]; //stores the current score in a String
-			int y = x - 1; //defines y each loop as one less than x
-				//shifts the elements that are numerically smaller (descending) than the currentMark one to the right
+			String currentName = numericalized[x].split(",")[0]; // stores the current name in a String
+			String currentMark = numericalized[x].split(",")[1]; // stores the current score in a String
+			int y = x - 1; // defines y each loop as one less than x
+			// shifts the elements that are numerically smaller (descending) than the
+			// currentMark one to the right
 			while ((y > -1) && Integer.parseInt(numericalized[y].split(",")[1]) < Integer.parseInt(currentMark)) {
 				numericalized[y + 1] = numericalized[y]; //
-				y--; 
+				y--;
 			}
-			numericalized[y + 1] = currentName + "," + currentMark; //inserts
+			numericalized[y + 1] = currentName + "," + currentMark; // inserts
 		}
-		return numericalized; //returns sorted array
+		return numericalized; // returns sorted array
 	}
-	
+
 	// ------------------------------------------------//
 
-	//method to display the data: uses one for loop and two enhanced for loops
-	public void displayData() {
-		//prints the original data from the file
+	// method to display the original data from the file
+	public void displayOriginalData() {
+		// prints the original data from the file
+		System.out.println("\n\n"); //space in case order varies
 		System.out.println("Original Data from the File: \n");
 		System.out.println("Score|Student");
 		for (int i = 0; i < LENGTH; i++) {
 			System.out.print(scores[i] + "     ");
 			System.out.println(students[i]);
 		}
-		
-		//prints the data in an alphabetized format
-		System.out.println("\n\n\n\n");
+
+	}
+
+	// method to display the alphabetized data
+	public void displayAlphabetizedData() {
+		// prints the data in an alphabetized format
+		System.out.println("\n\n"); //space in case order varies
 		System.out.println("Sorted Alphabetically: A-Z: \n");
 		System.out.println("Score|Student");
 		for (String str : sortAlphabetically()) {
-	
-		System.out.print(str.split(",")[1] + "     ");
+			System.out.print(str.split(",")[1] + "     ");
 			System.out.println(str.split(",")[0]);
 		}
-		
-		//prints the data in descending order (marks)
-		System.out.println("\n\n\n\n");
+	}
+
+	// method to display the data in descending order mark-wise
+	public void displayNumericalData() {
+		// prints the data in descending order (marks)
+		System.out.println("\n\n"); //space in case order varies
 		System.out.println("Sorted Numerically: Highest Average to Lowest Average: \n");
 		System.out.println("Score|Student");
 		for (String str : sortNumerically()) {
